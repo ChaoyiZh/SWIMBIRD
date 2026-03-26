d3f6f9f3aecc9fd1341257219416020299b80783
ChaoyiZh <chaoyizh@gmail.com>
2026-03-25 20:17:05 -0400

    commit from hp

--- VLMEvalKit/test.sh
+++ VLMEvalKit/test.sh
@@ -1,3 +1,3 @@
-export LMUData=/datasets/VLMEval
+export LMUData=/project/siyuh/common/chaoyi/code/SWIMBIRD/datasets/VLMEval
 
-CUDA_VISIBLE_DEVICES=0,1 torchrun  --master_port=29500 --nproc_per_node=2 run.py --data DynaMath WeMath MathVerse_MINI HRBench4K HRBench8K VStarBench MMStar RealWorldQA --model SwimBird-SFT-8B --judge your_judge_model --api-nproc 10 --verbose 
+CUDA_VISIBLE_DEVICES=0 torchrun  --master_port=29500 --nproc_per_node=1 run.py --data DynaMath WeMath MathVerse_MINI HRBench4K HRBench8K VStarBench MMStar RealWorldQA --model SwimBird-SFT-8B --judge your_judge_model --api-nproc 10 --verbose 
